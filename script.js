7c001781cceedd83f016a7ba9dc860be2a2418d3
Paul-Cte <paul.comte3878@gmail.com>
2025-03-29 15:25:19 +0100

    responsive
    responsive 99%, manque juste de bloquer le défilement quand le menu est actif pour pas qu'on puisse voir le footer

--- script.js
+++ script.js
@@ -18,3 +18,29 @@ window.addEventListener("scroll", function () {
         header.style.transform = "translateY(0)"; // Réaffiche le header
     }
 });
+
+const menu = document.getElementsByClassName('menu_deroulant')[0];
+
+
+
+document.getElementById("btn_burger").addEventListener("click", function () {
+    const isOpen = menu.classList.contains('menu_deroulant_active'); // Vérifie si le menu est ouvert
+    
+    menu.classList.toggle('menu_deroulant_active');
+
+    if (isOpen) {
+        body.classList.remove('bodyactive');
+        
+    } else {
+        body.classList.add('bodyactive');
+        
+    }
+});
+
+const closeMenu = () => {
+    menu.classList.remove('menu_deroulant_active');
+    body.classList.remove('bodyactive');
+};
+
+document.getElementById("btn_fermeture").addEventListener('click',closeMenu)
+
